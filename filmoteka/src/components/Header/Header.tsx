30e945eaa76c42e4f49bb2c76e0534c2c14d5c79
nikitanosov1 <nikinosov2002@mail.ru>
2022-12-20 01:08:28 +0400

    added header

---
+++ filmoteka/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './Header.module.css';
+
+export default function Header() {
+  return (
+    <div className={style.header}>
+        <div className={style.leftText}>Админка фильмотеки</div>
+        <div className={style.rightRectangle}>
+          <div className={style.rightText}>
+            Имя того кто это все закодил
+          </div>
+        </div>
+    </div>
+  )
+}
