5cf8f2dff9be8b4033c35ea4e7bdab50a05408da
nikitanosov1 <nikinosov2002@mail.ru>
2022-12-25 15:45:46 +0400

    fix shadow on header

--- filmoteka/src/components/Header/Header.tsx
+++ filmoteka/src/components/Header/Header.tsx
@@ -4,12 +4,14 @@ import style from './Header.module.css';
 export const Header = () => {
   return (
     <div className={style.header}>
-        <div className={style.leftText}>Админка фильмотеки</div>
-        <div className={style.rightRectangle}>
-          <div className={style.rightText}>
-            Имя того кто это все закодил
+      <div className={style.headerMain}>
+          <div className={style.leftText}>Админка фильмотеки</div>
+          <div className={style.rightRectangle}>
+            <div className={style.rightText}>
+              Имя того кто это все закодил
+            </div>
           </div>
-        </div>
+      </div>
     </div>
   )
 }
